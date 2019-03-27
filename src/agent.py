7359d83e3ff359cda491561f37848f0bff907dfe
jlrandulfe <jlrandulfe@gmail.com>
2019-03-27 17:14:40 +0100

    Calculate control variable and velocity in different functions

--- src/agent.py
+++ src/agent.py
@@ -14,10 +14,12 @@ class Agent(object):
     """
     def __init__(self, position, id):
         self.position = position
+        self.z = None
         self.id = id
         self.voronoi_cell = None
         self.centroid = None
         self.velocity = np.array([0, 0])
+        self.u = np.array([0, 0])
 
     def get_voronoi_cell(self, map_vertices, neighbours):
         """
@@ -35,16 +37,42 @@ class Agent(object):
         self.centroid = self.voronoi_cell.sum(axis=0) / len(self.voronoi_cell)
         return self.voronoi_cell
 
-    def get_velocity(self, Kp=1):
+    def get_control_variable(self, Kp=1):
+        """
+        Get the proportional control variable of the agent.
+        
+        :param float Kp: Proportional constant of the controller
+        :returns: 1xN float array, where N is the number of system
+         dimensions. Each element corresponds to the velocity in its
+         corresponding dimension.
+        """
         dist_to_centroid = self.centroid - self.position
-        self.velocity = dist_to_centroid * Kp
+        self.u = dist_to_centroid * Kp
+        return self.u
+
+    def get_velocity(self):
+        """
+        Use control variable for getting agent velocity
+        
+        :return: 1xN float array, where each element is the velocity in
+         each of the N dimensions.
+        """
+        self.velocity = self.u
         return self.velocity
 
     def move(self, delta_t):
         """
         Calculate new position of agent based on coverage control.
+
+        :param float delta_t: time step between the previous iteration
+         and the current one.
+        :returns: 1xN float array with the N new coordinates of the
+         agent position.
         """
+        # Apply control rule for getting the agent velocity.
+        self.get_control_variable()
         vel = self.get_velocity()
+        # Get the new position integrating the velocity through time.
         self.position += vel*delta_t
         return self.position
 
